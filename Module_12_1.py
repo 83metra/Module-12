b93ad4e281b065e7926e30fe35981c71c65bbc7c
83metra <jumo_004@mail.ru>
2024-09-21 19:15:56 +0200

    Add files via upload

---
+++ Module_12_1.py
@@ -0,0 +1,23 @@
+import runner, unittest
+
+class RunnerTest(unittest.TestCase):
+    def test_walk(self):
+        self.r1 = runner.Runner('Langsamer Karl')
+        for i in range(10):
+            self.r1.walk()
+        self.assertEqual(self.r1.distance, 50)
+
+    def test_run(self):
+        self.r2 = runner.Runner('Schneller Otto')
+        for i in range(10):
+            self.r2.run()
+        self.assertEqual(self.r2.distance, 100)
+
+    def test_callenge(self):
+        self.r3 = runner.Runner('Stille Moritz')
+        self.r4 = runner.Runner('Geschwindiger Siegfried')
+        for i in range(10):
+            self.r3.walk()
+            self.r4.run()
+        self.assertNotEqual(self.r3.distance, self.r4.distance)
+
